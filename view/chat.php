978cf4fe805ea7311358ec6c358238bf54b2c072
pras <pptech.ds@gmail.com>
2021-06-02 13:47:46 +0200

    get chat.php code from Camile

--- view/chat.php
+++ view/chat.php
@@ -1,19 +1,22 @@
 <table class="table table-hover">
     <thead>
         <tr>
-        <th scope="col" hidden>date</th>
-        <th scope="col" hidden>pseudo</th>
-        <th scope="col" hidden>message</th>
+            <th scope="col" class="col-2" hidden>date</th>
+            <th scope="col" class="col-2" hidden>pseudo</th>
+            <th scope="col" class="col-8" hidden>message</th>
         </tr>
-  </thead>
-  <tbody>
-<?php for ($i = 0; $i <= 5; $i++) { ?>
-    <tr class="table-light">
-      <td>date <?= $i ?></td>
-      <td>user <?= $i ?></td>
-      <td>message <?= $i ?></td>
-    </tr>
-<?php } ?>
-    
-  </tbody>
+    </thead>
+    <tbody>
+        <?php
+        for ($i = 0; $i < 5; $i++) {
+        ?>
+            <tr class="table-light">
+                <td>date <?= $i ?></td>
+                <td>user <?= $i ?></td>
+                <td>message <?= $i ?></td>
+            </tr>
+        <?php
+        }
+        ?>
+    </tbody>
 </table>

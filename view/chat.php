4742fc2c32932d946d2b6db517432170850fa60b
pras <pptech.ds@gmail.com>
2021-06-02 15:25:46 +0200

    message from BDD displayed in chat

--- view/chat.php
+++ view/chat.php
@@ -1,3 +1,35 @@
+<?php
+$user = 'root';
+$pass = '';
+$dbname = 'chat';
+$host = 'localhost';
+
+try {
+    $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
+    $options = array(
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    );
+    $dbh = new PDO($dsn, $user, $pass, $options);
+    // echo 'connection done <br>';
+} catch (PDOException $e) {
+    print "Error !: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+try {
+    $query = '  SELECT id, pseudo, content, date  
+                FROM message';
+
+    $req = $dbh->query($query);
+    $req ->setFetchMode(PDO::FETCH_ASSOC);
+    $tab = $req->fetchAll();
+    $req ->closeCursor();
+
+    // echo 'query done <br>';
+
+?>
+
 <table class="table table-hover">
     <thead>
         <tr>
@@ -8,15 +40,27 @@
     </thead>
     <tbody>
         <?php
-        for ($i = 0; $i < 5; $i++) {
+        foreach($tab as $num => $row) {
         ?>
             <tr class="table-light">
-                <td class="col-2">date <?= $i ?></td>
-                <td class="col-2">user <?= $i ?></td>
-                <td class="col-8">message <?= $i ?></td>
+                <td class="col-2"><?= $row['date'] ?></td>
+                <td class="col-2"><?= $row['pseudo'] ?></td>
+                <td class="col-8"><?= $row['content'] ?></td>
             </tr>
         <?php
         }
         ?>
     </tbody>
 </table>
+
+<?php
+
+    
+$dbh = null;
+    
+// echo 'End of Request !';
+} catch (PDOException $e) {
+print "Error !: " . $e->getMessage() . "<br/>";
+die();
+}
+?>

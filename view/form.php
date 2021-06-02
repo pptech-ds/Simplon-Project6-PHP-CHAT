f38200e54f6d9f78ff2432e756c08a93ceb5a45c
pras <pptech.ds@gmail.com>
2021-06-02 13:59:01 +0200

    get form.php code from Camile

--- view/form.php
+++ view/form.php
@@ -1,22 +1,18 @@
-<form method="POST" action="index.php">
+<form>
     <fieldset>
-        <table>
-            <tbody>
-                <tr>
-                    <td valign="top">
-                        <div class="form-group">
-                            <input type="text"  name="pseudo" class="form-control" id="pseudo" aria-describedby="pseudoHelp" placeholder="Entrez votre pseudo" required="">
-                        </div>
-                    </td>
-                    <td>
-                        <div class="form-group">
-                            <textarea name="message" class="form-control" id="message" rows="8" cols="45" placeholder="Entrez votre message" ></textarea>
-                        </div>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="row">
+            <div class="form-group col-4">
+                <label for="pseudo_fe" class="form-label mt-4" hidden>Pseudo</label>
+                <input type="text" class="form-control" id="pseudo_fe" aria-describedby="pseudo" placeholder="Entrez votre pseudo">
+            </div>
 
-        <button type="submit" name="send" value="Envoyer" class="btn btn-primary">Submit</button>
+            <div class="form-group col-8">
+                <label for="message_fe" class="form-label mt-4" hidden>message</label>
+                <textarea class="form-control" id="message_fe" rows="3" placeholder="Entrez votre message"></textarea>
+            </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Envoyer</button>
+
     </fieldset>
 </form>
